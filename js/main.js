3d77e37b3e4337dfb4a43ff484da2fc29baa3f32
Ivan1990Nik <pelevin.vanyusha90@mail.ru>
2024-02-15 23:40:55 +0500

    sclik

--- js/main.js
+++ js/main.js
@@ -6,6 +6,48 @@ $(function () {
     });
 });
 
+/* 
+$('.customers__slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: $('.customers__dots'),
+    waitForAnimate: false,
+   /*  // responsive:
+    //   [
+    //     {
+    //       breakpoint: 700,
+    //       settings: {
+
+    //       },
+    //     },
+    //   ] */
+
+
+/* $('.directions__filter-btn').on('click', function () {
+    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+    $(this).addClass('directions__filter-btn--active')
+}) */
+
+$('.customers__slider').slick({
+    arrows: false,
+    slidesToShow: 2,
+    infinite: true,
+    draggable: false,
+    waitForAnimate: false,
+    dots:true,
+
+})
+
+
+
+$('.customers__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.customers__slider').slick('slickPrev')
+})
+$('.customers__next').on('click', function (e) {
+    e.preventDefault()
+    $('.customers__slider').slick('slickNext')
+})
 
 
 
