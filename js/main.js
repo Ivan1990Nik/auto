b7af22e35802bf505d9c3d43788b9d6b5ea22fcd
Ivan1990Nik <pelevin.vanyusha90@mail.ru>
2024-02-16 23:42:17 +0500

    click

--- js/main.js
+++ js/main.js
@@ -6,27 +6,8 @@ $(function () {
     });
 });
 
-/* 
-$('.customers__slider').slick({
-    arrows: false,
-    dots: true,
-    appendDots: $('.customers__dots'),
-    waitForAnimate: false,
-   /*  // responsive:
-    //   [
-    //     {
-    //       breakpoint: 700,
-    //       settings: {
-
-    //       },
-    //     },
-    //   ] */
 
 
-/* $('.directions__filter-btn').on('click', function () {
-    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
-    $(this).addClass('directions__filter-btn--active')
-}) */
 
 $('.customers__slider').slick({
     arrows: false,
@@ -34,9 +15,10 @@ $('.customers__slider').slick({
     infinite: true,
     draggable: false,
     waitForAnimate: false,
-    dots:true,
-
+    dots: true,
+    appendDots: $('.customers__dots'),
 })
+
 
 
 
@@ -48,7 +30,6 @@ $('.customers__next').on('click', function (e) {
     e.preventDefault()
     $('.customers__slider').slick('slickNext')
 })
-
 
 
 
